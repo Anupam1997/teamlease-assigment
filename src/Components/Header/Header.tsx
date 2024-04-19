a8cdad598c878c07c20ed5c9b5223fd152836d84
Anupam Singh <anupamsingh215@gmail.com>
2024-04-19 21:29:06 +0530

    header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,12 +8,15 @@ function Header() {
         <h1 className="title">Site name</h1>
       </div>
       <nav className="nav-links">
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a className="nav-link" href="#">
           Page
         </a>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a className="nav-link" href="#">
           Page
         </a>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a className="nav-link" href="#">
           Page
         </a>
